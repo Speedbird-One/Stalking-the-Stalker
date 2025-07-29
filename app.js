6116c2b0a44817173e40f584820050ddd4df3982
Speedbird-One <tanmay.ta.tech@gmail.com>
2025-07-29 18:57:38 +0530

    Added assets and updated all files
    Added images and audio to the assets folder. Updated index.html, style.css and app.js files to finalize the website.

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ function dark_func1() {
       .forEach((elem) => (elem.style.color = fg_clr));
     document
       .querySelectorAll("a")
-      .forEach((elem) => (elem.style.color = "hsl(195, 50%, 80%)"));
+      .forEach((elem) => (elem.style.color = "hsl(195, 50%, 70%)"));
   }
 }
 
@@ -217,7 +217,7 @@ function dark_func2() {
       .forEach((elem) => (elem.style.color = fg_clr));
     document
       .querySelectorAll("a")
-      .forEach((elem) => (elem.style.color = "hsl(195, 50%, 80%)"));
+      .forEach((elem) => (elem.style.color = "hsl(195, 50%, 70%)"));
   }
 }
 
@@ -226,20 +226,50 @@ document.getElementById("isVulgar").addEventListener("change", vulgar_func1);
 function vulgar_func1() {
   if (document.getElementById("isVulgar").checked) {
     document.getElementById("isVulgar2").checked = true;
-    document.querySelector("#pic0").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic1").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic2").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic3").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic4").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic5").src = "assets/images/vulgar/dore_tp.gif";
+
+    document.getElementById("song").src = "assets/audio/mamtas-interlude.mp3";
+    document.getElementById("song").play();
+
+    document.querySelector("#pic0").src = "assets/images/vulgar/stalker.png";
+    document.querySelector("#pic1").src = "assets/images/vulgar/ramu-haram.jpg";
+    document.querySelector("#pic2").src = "assets/images/vulgar/9-11-haram.png";
+    document.querySelector("#pic3").src =
+      "assets/images/vulgar/fuk-ponnappa.jpg";
+
+    document.getElementById("head0").innerHTML = "WANTED: Stalker";
+    document.getElementById("par1").innerHTML =
+      "Turning 19 is big, bro! One more year and you'll be eligible to stalk people professionally - they call it 'market research' then. Till then, keep being Dharwad's most loyal follower (literally). Can't beleive you came out of your mom 19 years ago.";
+
+    document.getElementById("head2").innerHTML = "Only Slightly Suicidal";
+    document.getElementById("par2").innerHTML =
+      "I know that jump from GJB is tempting. It's almost as high as your parents were in November of 2005. But hold on, coz ISIS can't airstrike Jawahar without Llama Intelligence Agency's one and only Cheif Intelligence Officer.";
+
+    document.getElementById("head3").innerHTML = "Fuck You Ponnappa";
+    document.getElementById("par3").innerHTML =
+      "This one IS personal, kid. (Ran out of stuff to say and couldn't be arsed to write another paragraph. Only so many stalker references I can make before it gets boring.) So yeah... fuck you. Also, watch Star Wars you piece of shit!";
   } else {
     document.getElementById("isVulgar2").checked = false;
-    document.querySelector("#pic0").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic1").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic2").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic3").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic4").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic5").src = "assets/images/clean/Stalker.jpg";
+
+    document.getElementById("song").src = "assets/audio/happy-birthday.mp3";
+    document.getElementById("song").play();
+
+    document.querySelector("#pic0").src = "assets/images/clean/bday-boi.png";
+    document.querySelector("#pic1").src = "assets/images/clean/ramu-halal.jpg";
+    document.querySelector("#pic2").src = "assets/images/clean/9-11-halal.png";
+    document.querySelector("#pic3").src = "assets/images/clean/hecker.jpg";
+
+    document.getElementById("head0").innerHTML = "Birthday Boy";
+    document.getElementById("par1").innerHTML =
+      "Wishing you a very happy 19th birthday my man. Enjoy it while it lasts, because this is your last year before Leonardo DiCaprio stops returning your texts. Don't worry though, you'll get someone with that Vasundhara Krithika-rizz.";
+
+    document.getElementById("head2").innerHTML =
+      "Survived A Year In PESU... Almost";
+    document.getElementById("par2").innerHTML =
+      "Somehow you've avoided a spontaneous high jump from GJB terrace for almost as long as Surya has been alive. Hope you can manage 3 more years. May your attendance rise, your GPA stabilize and your bank account maximize.";
+
+    document.getElementById("head3").innerHTML = "Minecraft Hecker";
+    document.getElementById("par3").innerHTML =
+      "Bro be destroying 5 year olds in Minecraft with auto-clicker. I wish you 50 games of Bed Wars with 10+ kills. But like seriously bro, steal Damascus' PS5 and play some other games also lol.";
   }
 }
 
@@ -248,19 +278,49 @@ document.getElementById("isVulgar2").addEventListener("change", vulgar_func2);
 function vulgar_func2() {
   if (document.getElementById("isVulgar2").checked) {
     document.getElementById("isVulgar").checked = true;
-    document.querySelector("#pic0").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic1").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic2").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic3").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic4").src = "assets/images/vulgar/dore_tp.gif";
-    document.querySelector("#pic5").src = "assets/images/vulgar/dore_tp.gif";
+
+    document.getElementById("song").src = "assets/audio/mamtas-interlude.mp3";
+    document.getElementById("song").play();
+
+    document.querySelector("#pic0").src = "assets/images/vulgar/stalker.png";
+    document.querySelector("#pic1").src = "assets/images/vulgar/ramu-haram.jpg";
+    document.querySelector("#pic2").src = "assets/images/vulgar/9-11-haram.png";
+    document.querySelector("#pic3").src =
+      "assets/images/vulgar/fuk-ponnappa.jpg";
+
+    document.getElementById("head0").innerHTML = "WANTED: Stalker";
+    document.getElementById("par1").innerHTML =
+      "Turning 19 is big, bro! One more year and you'll be eligible to stalk people professionally - they call it 'market research' then. Till then, keep being Dharwad's most loyal follower (literally). Can't beleive you came out of your mom 19 years ago.";
+
+    document.getElementById("head2").innerHTML = "Only Slightly Suicidal";
+    document.getElementById("par2").innerHTML =
+      "I know that jump from GJB is tempting. It's almost as high as your parents were in November of 2005. But hold on, coz ISIS can't airstrike Jawahar without Llama Intelligence Agency's one and only Cheif Intelligence Officer.";
+
+    document.getElementById("head3").innerHTML = "Fuck You Ponnappa";
+    document.getElementById("par3").innerHTML =
+      "This one IS personal, kid. (Ran out of stuff to say and couldn't be arsed to write another paragraph. Only so many stalker references I can make before it gets boring.) So yeah... fuck you. Also, watch Star Wars you piece of shit!";
   } else {
     document.getElementById("isVulgar").checked = false;
-    document.querySelector("#pic0").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic1").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic2").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic3").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic4").src = "assets/images/clean/Stalker.jpg";
-    document.querySelector("#pic5").src = "assets/images/clean/Stalker.jpg";
+
+    document.getElementById("song").src = "assets/audio/happy-birthday.mp3";
+    document.getElementById("song").play();
+
+    document.querySelector("#pic0").src = "assets/images/clean/bday-boi.png";
+    document.querySelector("#pic1").src = "assets/images/clean/ramu-halal.jpg";
+    document.querySelector("#pic2").src = "assets/images/clean/9-11-halal.png";
+    document.querySelector("#pic3").src = "assets/images/clean/hecker.jpg";
+
+    document.getElementById("head0").innerHTML = "Birthday Boy";
+    document.getElementById("par1").innerHTML =
+      "Wishing you a very happy 19th birthday my man. Enjoy it while it lasts, because this is your last year before Leonardo DiCaprio stops returning your texts. Don't worry though, you'll get someone with that Vasundhara Krithika-rizz.";
+
+    document.getElementById("head2").innerHTML =
+      "Survived A Year In PESU... Almost";
+    document.getElementById("par2").innerHTML =
+      "Somehow you've avoided a spontaneous high jump from GJB terrace for almost as long as Surya has been alive. Hope you can manage 3 more years. May your attendance rise, your GPA stabilize and your bank account maximize.";
+
+    document.getElementById("head3").innerHTML = "Minecraft Hecker";
+    document.getElementById("par3").innerHTML =
+      "Bro be destroying 5 year olds in Minecraft with auto-clicker. I wish you 50 games of Bed Wars with 10+ kills. But like seriously bro, steal Damascus' PS5 and play some other games also lol.";
   }
 }
